a524a2d7e410e1a3cf664cf839ccb2bb3374ab92
Johnny Fuchs <johnnyfuchs@gmail.com>
2012-05-29 23:59:14 -0500

    added some prettying, routing

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,43 @@ var fs = require('fs');
  */
 
 exports.index = function(req, res){
-  var data = { title: 'E80;083', files : [] }
+  preload(function( pagedata ){
+    pagedata.main = "";
+    res.render('index', pagedata );
+  });
+};
+
+
+exports.blog = function(req, res){
+  var path = 'public/posts/'+req.params.entry+'.html';
+  preload(function( pagedata ) {
+    fs.stat(path, function( err, stats ){
+      if( err ){ pagedata.main = ""; res.render('index', pagedata );}
+      else {
+        fs.readFile( path, 'ascii', function(err, fcontents){
+          if( err ) { pagedata.main = "" }
+          else {
+            pagedata.main = fcontents.toString();
+          }
+          res.render('index', pagedata );
+        });
+      }
+    });
+  });
+};
+
+
+function preload( callback ){
+  var pagedata = { title: 'E80;083', files : [] }
   fs.readdir('public/posts/', function(err, files){
     for(i in files){
       if( files[i].match(/[a-zA-Z_]\.html/) ){
-        data.files.push( {
+        pagedata.files.push( {
           name: files[i].split('.')[0].replace(/_/gi, ' ').toUpperCase(),
           link: files[i].split('.')[0]
         });
       }
     }
-    res.render('index', data );
+    callback( pagedata );
   });
-};
-
-
+}
